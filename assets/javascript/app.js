cf699a43a0ce3a30137e3d583fa2ce894573624a
Tia Sirrine <tiasirrine@Tias-MacBook-Pro.local>
2018-03-03 23:27:00 -0700

    new new

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+//set of questions for game
+console.log("its working");
+
+var questions = [{
+    question: "In which state does Ben and Jerry's Company operate?",
+    answers: ["New York", "Vermont", "California", "Texas"],
+    correctAnswer: "Vermont", 
+    image:".."
+}, {
+    question: "Which flavor of icecream was the first in existence?",
+    answers: ["Vanilla", "Chocolate", "Strawberry/Cherry", "Honey"],
+    correctAnswer: "Strawberry/Cherry",
+    inmage: ".."
+}, {
+    question: "Which company created the flavor Bunny Tracks?",
+    answers: ["Haagen Daas", "Blue Bunny", "Dryers", "Ben and Jerry's"],
+    correctAnswer: "Blue Bunny",
+    image: ".."
+},
+{
+    question: "Which company was the first to sell Chocolate Chip Cookie Dough Icecream?",
+    answers: ["Haagen Daas", "Ben and Jerry's", "Bryers", "Blue Bunny"],
+    correctAnswer: "Ben and Jerry's",
+    image: ".."
+
+}];
+
+var game = {
+
+}
